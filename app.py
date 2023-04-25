f901c47c6cf87b8facc6422bbe51e6381a7f2a7f
Derek15235 <derek.jain@menloschool.org>
2023-04-24 17:09:21 -0700

    Completed Models and finished sign in/up

--- app.py
+++ app.py
@@ -7,16 +7,58 @@ app = Flask(__name__)
 app.secret_key = "l655TiS4OGaxoO17Fg=="
 
 @app.route("/", methods=["GET", "POST"])
-def login():
+def sign_in():
     if request.method == "GET":
         return render_template("sign_in.html")
     
-@app.route("/sign-up")
+    else:
+        if db_session.query(Student).where((Student.email == request.form["email"]) & (Student.password == request.form["password"])).first() is not None:
+            session["role"] = "student"
+            session["id"] = db_session.query(Student).where((Student.email == request.form["email"]) & (Student.password == request.form["password"])).first().id
+            return redirect(url_for("meetings"))
+        elif db_session.query(Teacher).where((Teacher.email == request.form["email"]) & (Teacher.password == request.form["password"])).first() is not None:
+            session["role"] = "teacher"
+            session["id"] = db_session.query(Teacher).where((Teacher.email == request.form["email"]) & (Teacher.password == request.form["password"])).first().id
+            return redirect(url_for("meetings"))
+        
+        flash("Incorrect Password or Email! Try Again", "error")
+        return render_template("sign_in.html")
+        
+
+
+    
+@app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "GET":
         return render_template("sign_up.html") 
+    else:
+        if db_session.query(Student).where((Student.email == request.form["email"]) & (Student.password == request.form["password"])).first() is not None:
+            flash("This User Already Exists! Try again")
+            return render_template("sign_up.html")
+        elif request.form["password"] != request.form["confirm"]:
+            flash("Passwords Do Not Match! Try again")
+            return render_template("sign_up.html")
+        session["role"] = "student"
+        db_session.add(Student(request.form["fname"], request.form["lname"], request.form["email"], request.form["password"]))
+        db_session.commit()
+        # Try to find simpler way
+        session["id"] = db_session.query(Student).where(Student.email == request.form["email"]).first().id
+        return redirect(url_for("meetings"))
+            
+    
+
+@app.route("/meetings", methods=["GET", "POST"])
+def meetings():
+    if request.method == "GET" and session["role"] == "student":
+        return render_template("student-meetings.html")
+    
+
+@app.before_first_request
+def setup():
+    init_db()
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
--- models.py
+++ models.py
@@ -9,3 +9,75 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 # TODO: Complete your models
+class Student(Base):
+    __tablename__ = "students"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    first_name = Column("first_name", TEXT, nullable=False)
+    last_name = Column("last_name", TEXT, nullable=False)
+    email = Column("email", TEXT, nullable=False)
+    password = Column("password", TEXT, nullable=False)
+
+    meetings = relationship("Meeting", back_populates="student")
+
+    # Constructor
+    def __init__ (self, first_name, last_name, email, password):
+        # id autoincrements
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return self.first_name + " " + self.last_name
+    
+class Teacher(Base):
+    __tablename__ = "teachers"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    first_name = Column("first_name", TEXT, nullable=False)
+    last_name = Column("last_name", TEXT, nullable=False)
+    email = Column("email", TEXT, nullable=False)
+    password = Column("password", TEXT, nullable=False)
+    img_link = Column("img_link", TEXT, nullable=False)
+    department = Column("department", TEXT, nullable=False)
+
+    meetings = relationship("Meeting", back_populates="teacher")
+
+    # Constructor
+    def __init__ (self, first_name, last_name, email, password, img_link, department):
+        # id autoincrements
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.img_link = img_link
+        self.department = department
+
+    def __repr__(self):
+        return self.first_name + " " + self.last_name
+
+class Meeting(Base):
+    __tablename__ = "meetings"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    date = Column("date", TEXT, nullable=False) 
+    time = Column("time", TEXT, nullable=False)
+    description = Column("description", TEXT)
+    teacher_id = Column("teacher_id", ForeignKey("teachers.id"), nullable=False)
+    student_id = Column("student_id", ForeignKey("students.id"))
+
+    student = relationship("Student", back_populates="meetings")
+    teacher = relationship("Teacher", back_populates="meetings")
+
+    # Constructor
+    def __init__(self, date, time, teacher_id, student_id=None, description=""):
+        self.date = date
+        self.time = time
+        self.teacher_id = teacher_id
+        self.student_id = student_id
+        self.description = description
+
